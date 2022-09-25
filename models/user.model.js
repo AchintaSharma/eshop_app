d29610a51c20292d816a4ec0c5b3e1365390b8ba
Achinta Sharma <23achinta@gmail.com>
2022-09-25 11:42:36 +0530

    Created user schema

--- models/user.model.js
+++ models/user.model.js
@@ -1,1 +1,40 @@
-const 
+const mongoose = require("mongoose");
+const constants = require("../utils/constants");
+
+//Create user schema
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    firstName : {
+        type : String,
+        required : true
+    },
+    lastName : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    phoneNumber : {
+        type : String,
+        required : true
+    },
+    role : {
+        type : String,
+        required : true,
+        default : constants.roles.user,
+        enum : [constants.roles.admin, constants.roles.user]
+    },
+    userName : {
+        type : String,
+        required : true,
+    }
+}, {timestamps : true, versionKey : false});
